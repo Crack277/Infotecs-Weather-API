0863be656d13c4ad2f9a65d428a45179ac1c2783
redun <redunsasha2017@gmail.com>
2026-07-21 23:20:20 +0700

    fix queue import

--- api/__init__.py
+++ api/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .routers.towns import router as town_router
 from .routers.users import router as user_router
+from .routers.weather import router as weather_router
 
 
 router = APIRouter()
 router.include_router(user_router)
+router.include_router(weather_router)
 router.include_router(town_router)
