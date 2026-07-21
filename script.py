fe8b1a5cbc6f337b9a947454bd80f55ebd60cb1d
redun <redunsasha2017@gmail.com>
2026-07-21 23:19:20 +0700

    period_update in lifespan

--- script.py
+++ script.py
@@ -1,8 +1,11 @@
+import asyncio
+
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from api import router as api_router
 from core import Base, db_helper
+from core.tasks.weather import period_update_weather
 
 
 @asynccontextmanager
@@ -10,7 +13,12 @@ async def lifespan(app: FastAPI):
     async with db_helper.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+    task = asyncio.create_task(period_update_weather())
+
     yield
+
+    task.cancel()
+    
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(api_router)
